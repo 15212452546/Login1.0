006296cbc48850fb09256798960008c17e3621f1
15212452546 <73045628+15212452546@users.noreply.github.com>
2021-03-09 20:12:17 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,18 @@
+package com;
+
+import com.entity.User;
+import com.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.log4j.lf5.util.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
